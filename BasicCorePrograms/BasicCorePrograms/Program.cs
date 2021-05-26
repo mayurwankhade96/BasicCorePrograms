41cda9949ae8bd343a1291f216eba689a92a6fc2
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-05-26 14:24:10 +0530

    [Mayur] Add : Welcome message added

---
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicCorePrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to basic core programs");
+        }
+    }
+}
